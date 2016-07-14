c30b6a39751a3403481d8afd84a2662f30285734
Mihai <mihaim94@gmail.com>
2016-07-14 17:50:56 +0300

    Day picker functional

--- src/test/java/com/selenium/pages/NewVacationRequestPage.java
+++ src/test/java/com/selenium/pages/NewVacationRequestPage.java
@@ -1,7 +1,10 @@
 package com.selenium.pages;
 
+import java.util.List;
+
 import org.openqa.selenium.WebElement;
 
+import net.serenitybdd.core.annotations.findby.By;
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
 
@@ -10,10 +13,61 @@ public class NewVacationRequestPage extends PageObject{
 	@FindBy (css = "a[href*='new-request']")
 	private WebElement newVacationRequest;
 	
+	@FindBy (css= ".Zebra_DatePicker_Icon_Wrapper input[name=startDate]")
+	private WebElement selectStartDate;
+	
+	@FindBy (css = "div.Zebra_DatePicker[style*='block']")
+	private WebElement monthContainer;
+	
+	@FindBy (css= ".Zebra_DatePicker_Icon_Wrapper input[name=endDate]")
+	private WebElement selectEndDate;
+	
+	
 	public void clickNewVacationRequest(){
 		newVacationRequest.click();
 	}
 	
+	public void clickOnStartDate(){
+		selectStartDate.click();	
+	}
+	
+	public void clickYear(){
+		WebElement datePicker = getDriver().findElement(By.cssSelector("div.Zebra_DatePicker[style*='block']"));
+		WebElement year = datePicker.findElement(By.cssSelector("td.dp_caption"));
+		year.click();
+	}
+	
+	public void seletYear(String myYear){
+		WebElement datePicker = getDriver().findElement(By.cssSelector("div.Zebra_DatePicker[style*='block']"));
+		WebElement clickNext = datePicker.findElement(By.cssSelector("td.dp_next"));
+		WebElement year = datePicker.findElement(By.cssSelector("td.dp_caption"));
+		while(!year.getText().equals(myYear))
+			clickNext.click();	
+	}
+	
+	public void selectMonth(String monthName){
+		List<WebElement> monthList = getDriver().findElements(By.cssSelector(".Zebra_DatePicker .dp_monthpicker tr td:not([class*=disabled])"));
+		for (WebElement webElement : monthList) {
+			if(webElement.getText().equals(monthName)){
+				webElement.click();
+				break;
+			}
+		}
+	}
+	
+	public void selectDay(String dayName){
+		List<WebElement> dayList = getDriver().findElements(By.cssSelector(".dp_daypicker[style*=display] td:not([class*=not]):not([class*=disable])"));
+		for (WebElement webElement : dayList) {
+			if(webElement.getText().equals(dayName)){
+				webElement.click();
+				break;
+			}
+		}
+	}
+	
+	public void clickOnEndDate(){
+	selectEndDate.click();	
+	}
 	
 
 }
--- src/test/java/com/selenium/steps/NewVacationRequestSteps.java
+++ src/test/java/com/selenium/steps/NewVacationRequestSteps.java
@@ -3,6 +3,7 @@ package com.selenium.steps;
 import com.selenium.pages.NewVacationRequestPage;
 
 import net.thucydides.core.annotations.Step;
+import net.thucydides.core.annotations.StepGroup;
 import net.thucydides.core.steps.ScenarioSteps;
 
 public class NewVacationRequestSteps extends ScenarioSteps {
@@ -13,4 +14,40 @@ public class NewVacationRequestSteps extends ScenarioSteps {
 	public void clickOnNewVacationRequest(){
 		newVacationRequestPage.clickNewVacationRequest();
 	}	
+	
+	@Step
+	public void clickOnStartDate(){
+		newVacationRequestPage.clickOnStartDate();
+	}
+	@Step
+	public void clickYear(){
+		newVacationRequestPage.clickYear();
+	}
+	@Step
+	public void seletYear(String myYear){
+		newVacationRequestPage.seletYear(myYear);
+	}
+	@Step
+	public void selectMonth(String monthName){
+		newVacationRequestPage.selectMonth(monthName);
+	}
+	
+	@Step
+	public void selectDay(String dayName){
+		newVacationRequestPage.selectDay(dayName);
+	}
+	
+	public void clickOnEndDate(){
+		newVacationRequestPage.clickOnEndDate();
+	}
+	
+	@StepGroup
+	public void selectDate(String myYear, String myMonth, String myDay){
+		clickYear();
+		seletYear(myYear);
+		selectMonth(myMonth);
+		selectDay(myDay);
+		
+		
+	}
 }
