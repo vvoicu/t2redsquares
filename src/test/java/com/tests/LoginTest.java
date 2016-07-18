df63dd7476518f5f8c900be5e7df15287571679a
paula <paulaklein20@gmail.com>
2016-07-18 14:45:01 +0300

    Login Test prepared for property files

--- src/test/java/com/tests/LoginTest.java
+++ src/test/java/com/tests/LoginTest.java
@@ -10,28 +10,23 @@ import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.Steps;
 
-//@UseTestDataFrom(value = Constans.CSV_FILE_PATH +"Test00_LogIn.csv")
-
-
 //@RunWith(SerenityParameterizedRunner.class)
 //@UseTestDataFrom(value = Constants.CSV_FILE_PATH + "Test00_LogIn.csv")
 
 @RunWith(SerenityRunner.class)
 public class LoginTest extends BaseTest {
+	
 	@Managed(uniqueSession = true)
 	public WebDriver webdriver;
 	
-	
 	@Steps LoginSteps loginSteps;
-	
 
 //	private String username = "attila.marton";
 //	private String password = "test";
 
-	private String username;
-	private String password;
+//	private String username;
+//	private String password;
 
-	
 	@Test
 	public void loginTest() {
 		loginSteps.perform_login(username, password);
