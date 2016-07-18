8a6de1e8f972111b9f5d9ac287e1e1d33dc0737a
Attila <attila.marton@evozon.com>
2016-07-18 14:49:34 +0300

    dsvsdvds

--- src/test/java/com/tests/LoginTest.java
+++ src/test/java/com/tests/LoginTest.java
@@ -5,26 +5,22 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 
 import com.selenium.steps.LoginSteps;
-import com.tools.models.Constants;
 
-import net.serenitybdd.junit.runners.SerenityParameterizedRunner;
+import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.Steps;
-import net.thucydides.junit.annotations.UseTestDataFrom;
 
-@RunWith(SerenityParameterizedRunner.class)
-@UseTestDataFrom(value = Constants.CSV_FILE_PATH + "Test00_LogIn.csv")
-public class LoginTest {
+@RunWith(SerenityRunner.class)
+public class LoginTest{
 
 	@Managed(uniqueSession = true)
-	public WebDriver webdriver;
-	
-	
+	WebDriver webdriver;
+
 	@Steps LoginSteps loginSteps;
-	
-	private String username;
-	private String password;
-	
+
+//	private String username = "attila.marton";
+//	private String password = "test";
+
 	@Test
 	public void loginTest() {
 		loginSteps.perform_login(username, password);
