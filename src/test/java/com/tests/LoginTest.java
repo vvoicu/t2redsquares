1cb3b1ec48bbafffdd98569e6ac13359d702f5a2
Mihai <mihaim94@gmail.com>
2016-07-18 10:23:46 +0300

    csv new vacation request

--- src/test/java/com/tests/LoginTest.java
+++ src/test/java/com/tests/LoginTest.java
@@ -5,31 +5,28 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 
 import com.selenium.steps.LoginSteps;
+import com.tools.models.Constants;
 
 import net.serenitybdd.junit.runners.SerenityParameterizedRunner;
 import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.Steps;
+import net.thucydides.junit.annotations.UseTestDataFrom;
 
 @RunWith(SerenityParameterizedRunner.class)
-//@UseTestDataFrom(value = Constans.CSV_FILE_PATH +"Test00_LogIn.csv")
+@UseTestDataFrom(value = Constants.CSV_FILE_PATH + "Test00_LogIn.csv")
 public class LoginTest {
 
 	@Managed(uniqueSession = true)
 	public WebDriver webdriver;
 	
+	
 	@Steps LoginSteps loginSteps;
 	
-	private String username = "attila.marton";
-	private String password = "test";
+	private String username;
+	private String password;
 	
 	@Test
-	
 	public void loginTest() {
-		/*loginSteps.open_login_page();
-		loginSteps.load_signin_page();
-		loginSteps.enter_username(username);
-		loginSteps.enter_password(password);
-		loginSteps.click_signin_button();*/
 		loginSteps.perform_login(username, password);
 		loginSteps.verify_login();
 	}
--- src/test/java/com/tools/models/Constants.java
+++ src/test/java/com/tools/models/Constants.java
@@ -2,7 +2,8 @@ package com.tools.models;
 
 public class Constants {
 	
-	public static final String CSV_FILE_PATH = "/tests/csv/";
+	public static final String CSV_FILE_PATH = "src/test/resources/tests/csv/";
 	public static final char CSV_SEPARATOR = ',';
+	//public static final String BASE_URL= "http://172.22.4.88:9090/home";
 
 }
