defe7ba0b4b40f9b67a7265db4a8df3373bd02c5
Mihai <mihaim94@gmail.com>
2016-07-19 15:29:08 +0300

    Mail

---
+++ src/test/java/com/tools/models/Mail.java
@@ -0,0 +1,51 @@
+package com.tools.models;
+
+import java.util.Properties;
+
+import javax.mail.BodyPart;
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Store;
+
+public class Mail {
+
+	public static void main(String[] args) {
+		Properties props = new Properties();
+		props.setProperty("mail.store.protocol", "imaps");
+		props.setProperty("mail.imaps.port", "993");
+		try {
+			Session session = Session.getInstance(props, null);
+			Store store = session.getStore();
+			store.connect("mail.evozon.com", "marian.mihai@evozon.com", "OlimpiA17@");
+			Folder inbox = store.getFolder("INBOX");
+			inbox.open(Folder.READ_ONLY);
+
+			Message msgs[] = inbox.getMessages();
+			System.out.println("messages.length---" + msgs.length);
+
+			for (Message msg : msgs) {
+				if (msg.getContentType().contains("multipart")) {
+					Multipart mp = (Multipart) msg.getContent();
+					BodyPart bp = mp.getBodyPart(0);
+
+					System.out.println("===============================================================");
+
+					System.out.println("FROM:" + msg.getFrom()[0]);
+					System.out.println("SENT DATE:" + msg.getSentDate());
+					System.out.println("SUBJECT:" + msg.getSubject());
+					System.out.println("CONTENT:" + bp.getContent());
+				} else {
+					System.out.println("FROM:" + msg.getFrom()[0]);
+					System.out.println("SENT DATE:" + msg.getSentDate());
+					System.out.println("SUBJECT:" + msg.getSubject());
+					System.out.println("CONTENT:" + msg.getContent());
+
+				}
+			}
+		} catch (Exception mex) {
+			mex.printStackTrace();
+		}
+	}
+}
