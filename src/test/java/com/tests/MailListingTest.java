32c06626584f754c6a52d139efb2263cdd42dd5c
Attila <attila.marton@evozon.com>
2016-07-19 17:08:02 +0300

    Commited the page,steps,test for the mail listing

---
+++ src/test/java/com/tests/MailListingTest.java
@@ -0,0 +1,29 @@
+package com.tests;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.selenium.steps.MailListingSteps;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Steps;
+
+@RunWith(SerenityRunner.class)
+public class MailListingTest {
+	
+	private String host="mail.evozon.com";
+	private String username="attila.marton@evozon.com";
+	private String password="Shippuuden9.";
+	private String storeType="imaps";
+//	true = shows read mails; false = shows unread mails
+	boolean read=false;
+//  tickOption = tick => ticks mails as read
+	private String tickOption="  ";
+	
+	@Steps MailListingSteps mailListingSteps;
+	
+	@Test
+	public void mailListingTest(){
+		mailListingSteps.mailListingPage(host, storeType, username, password, read, tickOption);
+	}
+}
